56b89cc965752bc2e653005604cef899a981efa3
suyash-mankar <suyashmankar@gmail.com>
2022-10-15 17:54:38 +0530

    add hooks in create-post

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CreatePost() {
+
+    const [title, setTitle] = useState('');
+    const [subTitle, setSubTitle] = useState('');
+    const [content, setContent] = useState('');
+
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log('title', title);
+        console.log('subTitle', subTitle);
+        console.log('content', content);
+    }
+
     return (
-        <div>
-            CreatePost
+        <div className='create-post'>
+            <h1> Create Post</h1>
+            <form onSubmit={handleSubmit}> 
+                <div className='form-field'> 
+                    <label> Title </label>
+                    <input value={title} onChange={(e)=> setTitle(e.target.value)} />
+                    {/* e.target.value is whatever the user is typing */}
+
+                </div>
+                <div className='form-field'> 
+                    <label> SubTitle </label>
+                    <input value={subTitle} onChange={(e)=> setSubTitle(e.target.value)} />
+                </div>
+                <div className='form-field'> 
+                    <label> Content </label>
+                    <textarea value={content} onChange={(e)=> setContent(e.target.value)}>  </textarea>
+                </div>
+
+                <button className='create-post-btn'> Create Post </button>
+            </form>
         </div>
     )
 }
