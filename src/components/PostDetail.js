6c067e85bfdab1c97161491d715d4e3a1c9ff430
suyash-mankar <suyashmankar@gmail.com>
2022-10-16 13:06:36 +0530

    setup post detail component

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../firebase';
+import { useParams } from 'react-router-dom';
+
 
 function PostDetail() {
+
+    const [post, setPost] = useState({});
+    const { postId } = useParams();
+
+    useEffect(() => {
+        firestore
+            .collection('posts')
+            .doc(postId)
+            .get()
+            .then((snapshot) => {
+                console.log('snapshot', snapshot.data());
+                setPost(snapshot.data());
+            })
+
+    }, []);
+
+
     return (
-        <div>
-            PostDetail
+        <div className="post-detail">
+            <h1> {post.title} </h1>
+            <p> {post.content} </p>
         </div>
     )
 }
